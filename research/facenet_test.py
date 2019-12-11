87bf90e18cf74c4d3565f3477b5dfc1779516d5c
mi-shin <shinpei.mine@ghelia.com>
2019-12-11 09:07:17 +0000

    #10 #11 #13 first commit

--- research/facenet_test.py
+++ research/facenet_test.py
@@ -7,9 +7,15 @@ import torchvision.transforms as T
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import torch
 
 # %%
-mtcnn = MTCNN(image_size=1440, margin=0)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device
+
+# %%
+# thresholds = [0.5, 0.6, 0.6]
+mtcnn = MTCNN(image_size=1440, margin=0, keep_all=True, device=device, thresholds=thresholds)
 
 # %%
 img_path = "image_00032.png"
@@ -22,14 +28,16 @@ display(T.ToPILImage()(img_cropped))
 # %%
 rects, probs, landmarks = mtcnn.detect(img, landmarks=True)
 img_copy = img.copy()
-draw = ImageDraw.Draw(img_copy)
+
+img_copy = np.asarray(img_copy)
 for i in range(len(rects)):
-    print(i, probs[i])
-    draw.rectangle(rects[i].tolist(), outline="blue")
-    # draw.point(landmarks[i], fill="blue")
-    img_copy = np.asarray(img_copy)
-    cv2.drawMarker(img_copy, position, "blue", markerSize=20)
-    img_copy = Image.fromarray(img_copy)
+    for landmark in landmarks[i]:
+        cv2.drawMarker(img_copy, tuple(map(int, landmark.tolist())), (0,0,255), markerSize=10)
+
+img_copy = Image.fromarray(img_copy)
+draw = ImageDraw.Draw(img_copy)
+for rect in rects:
+    draw.rectangle(tuple(rect.tolist()), outline="green")
 display(img_copy)
 # %%
 img_copy.save("output_facenet.png")
